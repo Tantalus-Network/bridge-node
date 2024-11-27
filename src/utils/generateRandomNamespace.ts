52bfa8abedcb536b93d4102a66edb5700f53f474
Feyyaz Numan Cavlak <feyyazcavlak@outlook.com>
2024-11-27 22:00:23 +0300

    Add random namespace

---
+++ src/utils/generateRandomNamespace.ts
@@ -0,0 +1,19 @@
+import { sha256 } from "js-sha256";
+
+export default (): string => {
+    const seed = Math.random().toString();
+    const namespaceVersionByte = Buffer.from([0x00]);
+
+    const leadingZeroBytes = Buffer.alloc(18, 0);
+
+    const IdHash = sha256(seed);
+    const slicedIdHash = Buffer.from(IdHash).subarray(0, 10);
+
+    const namespaceIdentifier = Buffer.concat([
+        namespaceVersionByte,
+        leadingZeroBytes,
+        slicedIdHash,
+    ]).toString("base64");
+
+    return namespaceIdentifier;
+};
